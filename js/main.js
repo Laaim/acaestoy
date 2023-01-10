33d20ddb4dbc64e42d60018448e9ae4aaecacc2f
lai <laila.magallan@hotmail.com>
2023-01-10 14:47:47 -0300

    formulario añadido en mascotas encontradas y añadido js para el filtro

--- js/main.js
+++ js/main.js
@@ -1,29 +1,60 @@
-<script>
-  // Obtener todas las tarjetas
-  let cards = document.querySelectorAll(".card");
+// JavaScript
+function filterResults() {
+  // Obtenemos el valor escrito en la barra de búsqueda
+  var searchValue = document.getElementById('search').value.toLowerCase();
 
-  // Obtener el botón de filtro
-  let filterButton = document.querySelector("#filterButton");
+  // Obtenemos los valores seleccionados en los filtros
+  var petType = document.getElementById('pet-type').value;
+  var petCity = document.getElementById('pet-city').value;
+  var petColor = document.getElementById('pet-color').value;
 
-  // Agregar un evento de clic al botón de filtro
-  filterButton.addEventListener("click", function() {
-    // Obtener el tipo de filtro seleccionado
-    let filterType = document.querySelector("#filterSelect").value;
+  // Iteramos sobre cada una de las respuestas del formulario
+  var responses = document.querySelectorAll('.jf-response');
+  for (var i = 0; i < responses.length; i++) {
+    var response = responses[i];
 
-    // Recorrer todas las tarjetas
-    for (let card of cards) {
-      // Si el tipo de filtro es "todos", mostrar todas las tarjetas
-      if (filterType == "todos") {
-        card.style.display = "block";
-      } else {
-        // Si el tipo de la tarjeta no coincide con el filtro, ocultarla
-        if (card.getAttribute("data-type") != filterType) {
-          card.style.display = "none";
-        } else {
-          // Si el tipo de la tarjeta coincide con el filtro, mostrarla
-          card.style.display = "block";
-        }
-      }
+    // Mostramos o ocultamos la respuesta según corresponda
+    if (matchesFilters(response, searchValue, petType, petCity, petColor)) {
+      response.style.display = 'block';
+    } else {
+      response.style.display = 'none';
     }
-  });
-</script>
+  }
+}
+
+function matchesFilters(response, searchValue, petType, petCity, petColor) {
+  // Comprobamos si la respuesta coincide con el valor escrito en la barra de búsqueda
+  if (searchValue) {
+    var description = response.querySelector('.jf-description').textContent.toLowerCase();
+    if (description.indexOf(searchValue) === -1) {
+      return false;
+    }
+  }
+
+  // Comprobamos si la respuesta coincide con el tipo de mascota seleccionado
+  if (petType) {
+    var type = response.querySelector('.jf-pet-type').textContent.toLowerCase();
+    if (type !== petType) {
+      return false;
+    }
+  }
+
+  // Comprobamos si la respuesta coincide con la ciudad seleccionada
+  if (petCity) {
+    var city = response.querySelector('.jf-pet-city').textContent.toLowerCase();
+    if (city !== petCity) {
+      return false;
+    }
+  }
+
+  // Comprobamos si la respuesta coincide con el color de pelaje seleccionado
+  if (petColor) {
+    var color = response.querySelector('.jf-pet-color').textContent.toLowerCase();
+    if (color !== petColor) {
+      return false;
+    }
+  }
+
+  // Si la respuesta coincide con todos los filtros, la devolvemos
+  return true;
+}
